76e3d5c37b0e3dece58bab31ee2e3512f1041805
Frank Boucher <2404846+FBoucher@users.noreply.github.com>
2024-05-03 10:54:46 -0400

    31 Pick a door to proceed to the next room (#33)
    * Add play and fix a few things
    
    - fix save id =0
    - use AdventurerCard component
    - Add Play page
    - Move Canvas invokes in a static class
    
    * Dictionary<char,Exit>
    
    * Add pick a Door dialog
    
    * door are selectable

---
+++ src/2d6-dungeon-service/domain/DoorSelections.cs
@@ -0,0 +1,13 @@
+﻿namespace c5m._2d6Dungeon;
+
+public class DoorSelections
+{
+    public Dictionary<char, Exit> Exits { get; set; }
+
+    public Exit? SelectedExit { get; set; } = null;
+
+    public DoorSelections(Dictionary<char, Exit> exits){
+        Exits = exits;
+    }
+
+}
