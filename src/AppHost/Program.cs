be4d5e4f5212c348e03635e334fce071c0681ab6
Frank <fboucher@outlook.com>
2025-05-06 07:40:56 -0400

    add full path on mounts

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -6,10 +6,10 @@ var mysql = builder.AddMySql("mysql")
                 
 var db2d6 = mysql.AddDatabase("db2d6");
 
-mysql.WithBindMount("../../database/scripts/schema.sql", "/docker-entrypoint-initdb.d/1.sql")
-     .WithBindMount("../../database/scripts/data.sql", "/docker-entrypoint-initdb.d/2.sql");
+mysql.WithBindMount(GetFullPath("../../database/scripts/schema.sql"), "/docker-entrypoint-initdb.d/1.sql")
+     .WithBindMount(GetFullPath("../../database/scripts/data.sql"), "/docker-entrypoint-initdb.d/2.sql");
                         
-var dab = builder.AddDataAPIBuilder("dab", ["../../database/dab-config.json"])
+var dab = builder.AddDataAPIBuilder("dab", [GetFullPath("../../database/dab-config.json")])
                 .WithReference(db2d6)
                 .WaitFor(db2d6);
 
@@ -19,3 +19,7 @@ builder.AddProject<Projects._2d6_dungeon_web_client>("webapp")
         .WithExternalHttpEndpoints(); 
 
 builder.Build().Run();
+
+
+static string GetFullPath(string relativePath) =>
+    Path.GetFullPath(Path.Combine(Projects.AppHost.ProjectPath, relativePath));
