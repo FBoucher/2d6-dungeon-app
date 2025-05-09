35deccfade452e652c099843540403ec4c79f9c8
Frank <fboucher@outlook.com>
2025-05-08 21:41:53 -0400

    trying WithInitBindMount

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -9,7 +9,9 @@ var db2d6 = mysql.AddDatabase("db2d6");
 // mysql.WithBindMount(source: "../../database/scripts/schema.sql", target: "/docker-entrypoint-initdb.d/1.sql")
 //      .WithBindMount(source: "../../database/scripts/data.sql",  target: "/docker-entrypoint-initdb.d/2.sql");
 
-mysql.WithBindMount(source: "../../database/scripts", target: "/docker-entrypoint-initdb.d");
+mysql.WithInitBindMount(source: "../../database/scripts", isReadOnly: true);
+
+//mysql.WithBindMount(, target: "/docker-entrypoint-initdb.d");
 
 
 var dab = builder.AddDataAPIBuilder("dab", ["../../database/dab-config.json"])
