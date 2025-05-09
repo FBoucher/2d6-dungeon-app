603607bdb82dbfca19b4807a2e2b903d7307d36f
Frank <fboucher@outlook.com>
2025-05-08 21:21:21 -0400

    Updates database initialization in AppHost
    Refactors the AppHost configuration to streamline database
    initialization by mounting the entire database scripts directory
    instead of individual files. This simplifies the configuration
    and improves maintainability.
    
    Renames database scripts for clarity and proper ordering
    during initialization.

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,14 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var mysql = builder.AddMySql("mysql")
+var mysql = builder.AddMySql("sqlsvr2d6")
                    //.WithPhpMyAdmin()
                    .WithLifetime(ContainerLifetime.Persistent);
                 
 var db2d6 = mysql.AddDatabase("db2d6");
 
-mysql.WithBindMount(source: "../../database/scripts/schema.sql", target: "/docker-entrypoint-initdb.d/1.sql")
-     .WithBindMount(source: "../../database/scripts/data.sql",  target: "/docker-entrypoint-initdb.d/2.sql");
-                        
+// mysql.WithBindMount(source: "../../database/scripts/schema.sql", target: "/docker-entrypoint-initdb.d/1.sql")
+//      .WithBindMount(source: "../../database/scripts/data.sql",  target: "/docker-entrypoint-initdb.d/2.sql");
+
+mysql.WithBindMount(source: "../../database/scripts/", target: "/docker-entrypoint-initdb.d/");
+
+
 var dab = builder.AddDataAPIBuilder("dab", ["../../database/dab-config.json"])
                 .WithReference(db2d6)
                 .WaitFor(db2d6);
