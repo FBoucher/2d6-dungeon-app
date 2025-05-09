2fb876798920fc23341184aa65924512110e500b
Frank <fboucher@outlook.com>
2025-05-08 20:44:40 -0400

    Simplifies and clarifies AppHost configuration.
    Removes unnecessary deployment files.
    
    Streamlines the AppHost configuration by removing the `GetFullPath` function and directly referencing the paths for bind mounts and the Data API Builder configuration file. This improves readability and reduces code complexity.

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -6,10 +6,10 @@ var mysql = builder.AddMySql("mysql")
                 
 var db2d6 = mysql.AddDatabase("db2d6");
 
-mysql.WithBindMount(GetFullPath("../../database/scripts/schema.sql"), "/docker-entrypoint-initdb.d/1.sql")
-     .WithBindMount(GetFullPath("../../database/scripts/data.sql"), "/docker-entrypoint-initdb.d/2.sql");
+mysql.WithBindMount(source: "../../database/scripts/schema.sql", target: "/docker-entrypoint-initdb.d/1.sql")
+     .WithBindMount(source: "../../database/scripts/data.sql",  target: "/docker-entrypoint-initdb.d/2.sql");
                         
-var dab = builder.AddDataAPIBuilder("dab", [GetFullPath("../../database/dab-config.json")])
+var dab = builder.AddDataAPIBuilder("dab", ["../../database/dab-config.json"])
                 .WithReference(db2d6)
                 .WaitFor(db2d6);
 
@@ -21,5 +21,5 @@ builder.AddProject<Projects._2d6_dungeon_web_client>("webapp")
 builder.Build().Run();
 
 
-static string GetFullPath(string relativePath) =>
-    Path.GetFullPath(Path.Combine(Projects.AppHost.ProjectPath, relativePath));
+// static string GetFullPath(string relativePath) =>
+//     Path.GetFullPath(Path.Combine(Projects.AppHost.ProjectPath, relativePath));
